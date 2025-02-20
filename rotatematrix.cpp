026a44fe164e37e72a7358f70c5f6bd207cb3629
Himani <arorahimani080@gmail.com>
2025-02-20 15:29:21 +0530

    printcount

---
+++ rotatematrix.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<vector<int>> matrix;
+    for(int i=0;i<matrix.size()-1;i++){
+         for(int j=0;j<matrix.size()-2;j++){
+           
+         }
+    }
+
+}
